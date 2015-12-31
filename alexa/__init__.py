7b85d52b2e4ab2cb36f450078c4fb91d8e811eb7
Timothy Messier <tim.messier@gmail.com>
2015-12-31 13:20:24 -0500

    cleanup

---
+++ alexa/__init__.py
@@ -0,0 +1,49 @@
+# vim: set et ts=4 sw=4 fileencoding=utf-8:
+'''
+alexa
+=====
+'''
+
+
+class Response(object):
+    '''
+    Build an Alexa response
+    '''
+    def __init__(self,
+                 output,
+                 title=None,
+                 reprompt_text=None,
+                 should_end_session=True,
+                 session_attributes=None):
+        self.output = output
+        self.title = title or 'Plex'
+        self.reprompt_text = reprompt_text
+        self.should_end_session = should_end_session
+        self.session_attributes = session_attributes or {}
+
+    def to_dict(self):
+        '''
+        Convert to expected dict response.
+        '''
+        return {
+            'version': '1.0',
+            'sessionAttributes': self.session_attributes,
+            'response': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': self.output
+                },
+                'card': {
+                    'type': 'Simple',
+                    'title': 'SessionSpeechlet - ' + self.title,
+                    'content': 'SessionSpeechlet - ' + self.output
+                },
+                'reprompt': {
+                    'outputSpeech': {
+                        'type': 'PlainText',
+                        'text': self.reprompt_text
+                    }
+                },
+                'shouldEndSession': self.should_end_session
+            }
+        }
--- aplexa.py
+++ aplexa.py
@@ -5,73 +5,49 @@ aplexa
 '''
 from __future__ import print_function
 
+import logging
+
 import dotenv
 from getenv import env
 from plexapi.myplex import MyPlexUser
 
+from alexa import Response
+
 dotenv.read_dotenv()
 
+logger = logging.getLogger(__name__)
 PLEX_USER = MyPlexUser.signin(env('PMS_USERNAME'), env('PMS_PASSWORD'))
 PLEX = PLEX_USER.getResource(env('PMS_SERVERNAME')).connect()
 
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': 'SessionSpeechlet - ' + title,
-            'content': 'SessionSpeechlet - ' + output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
-
-
-def build_response(session_attributes, speechlet_response):
-    return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
-    }
-
-
 def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
-
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Plex is listening.."
-    reprompt_text = None
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    '''
+    Get the welcome response when waiting for more.
+    '''
+    return Response(
+        'Plex is listening..',
+        'Welcome',
+        should_end_session=False,
+    ).to_dict()
 
 
 def get_on_deck(intent, session):
-    session_attributes = {}
-    card_title = "On Deck"
-    speech_output = "On deck"
-    reprompt_text = None
-    should_end_session = True
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    '''
+    Get the shows on deck.
+    '''
+    logger.info('get_on_deck intent_name=%s, sessionId=%s',
+                intent['name'],
+                session['sessionId'])
+    return Response('On deck', 'On Deck').to_dict()
 
 
 def start_movie(intent, session):
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
+    '''
+    Start playing a movie.
+    '''
+    logger.info('start_movie intent_name=%s, sessionId=%s',
+                intent['name'],
+                session['sessionId'])
     if 'showName' in intent['slots']:
         show = intent['slots']['showName']['value']
         movie = PLEX.library.section('Movies').get(show)
@@ -81,73 +57,69 @@ def start_movie(intent, session):
         speech_output = 'Started movie ' + show
     else:
         speech_output = 'No show specified'
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+    return Response(speech_output, intent['name']).to_dict()
 
 
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
+def on_session_started(request, session):
+    '''
+    Called when the session starts
+    '''
+    logger.info('on_session_started requestId=%s, sessionId=%s',
+                request['requestId'],
+                session['sessionId'])
 
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
 
-
-def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-
-    print("on_launch requestId=" + launch_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # Dispatch to your skill's launch
+def on_launch(request, session):
+    '''
+    Called when the user launches the skill without specifying what they want
+    '''
+    logger.info('on_launch requestId=%s, sessionId=%s',
+                request['requestId'],
+                session['sessionId'])
     return get_welcome_response()
 
 
-def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
+def on_session_ended(request, session):
+    '''
+    Called when the user ends the session.
 
-    Is not called when the skill returns should_end_session=true
-    """
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
+    Is not called when the skill returns ``should_end_session=true``
+    '''
+    logger.info('on_session_ended requestId=%s, sessionId=%s',
+                request['requestId'],
+                session['sessionId'])
 
 
-def on_intent(intent_request, session):
-    """ Called when the user specifies an intent for this skill """
+def on_intent(request, session):
+    '''
+    Called when the user specifies an intent for this skill
+    '''
+    logger.info('on_intent requestId=%s, sessionId=%s',
+                request['requestId'],
+                session['sessionId'])
+    intent = request['intent']
+    intent_name = request['intent']['name']
 
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
-
-    # Dispatch to your skill's intent handlers
     if intent_name == "OnDeckIntent":
         return get_on_deck(intent, session)
     elif intent_name == "StartShowIntent":
         return start_movie(intent, session)
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
     else:
         raise ValueError("Invalid intent")
 
 
-def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
-    """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
+def lambda_handler(event, context):  # pylint: disable=unused-argument
+    '''
+    Main entry point for aws lamba.
 
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    Routes the incoming request based on type. The JSON body of the
+    request is provided in the event parameter.
+    '''
+    event_app_id = event['session']['application']['applicationId']
+    logger.info('event.session.application.applicationId=%s', event_app_id)
+
+    if event_app_id != 'amzn1.echo-sdk-ams.app.{}'.format(env('ALEXA_APP_IP')):
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
